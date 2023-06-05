6de2647b539436dae45f1a9e05efac959e8ac05d
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-06-05 16:30:05 +0200

    add store and provider redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,22 @@ import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
 import AppRoutes from "./routes"
 import { Provider } from "react-redux"
-/* import store from "./store/store" */
+import store from "./store/store" 
 function App() {
   return (
+
     <div className="container">
-
-{/*       <Provider store={store}> */}
-      <div className="Navbar">
-        <NavBar />
-      </div>
-
-      <div className="Body">
-        <AppRoutes />
-      </div>
-
-      <div className="Footer">
-        <Footer />
-      </div>
-
-{/*       </Provider>*/}
-
+       <Provider store={store}> 
+          <div className="Navbar">
+            <NavBar />
+          </div>
+          <div className="Body">
+            <AppRoutes />
+          </div>
+          <div className="Footer">
+            <Footer />
+          </div>
+        </Provider>
     </div>
   )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,5 @@
-/* import { configureStore } from "@reduxjs/toolkit";
-
-//import userReducer
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
 
 const store = configureStore({
   reducer: {
@@ -11,4 +10,3 @@ const store = configureStore({
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
- */
