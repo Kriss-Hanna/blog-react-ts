012fd01a4098a05b64bbd6b9320de3eea9ce7bce
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-08-17 14:32:57 +0200

    add component Admin

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,16 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+/* import { Link } from "react-router-dom"; */
 
-function Admin() {
-  return (
-    <div>
-        <h2>Admin</h2>
-        <form action="/admin" method="post">
-            <input type="text" name="username" placeholder="username" />
-            <input type="password" name="password" placeholder="password" />
-            
-        </form>
-    </div>
-  )
+interface LoginProps {
+  onLogin: (email: string, password: string) => void;
 }
 
-export default Admin
+const Admin: React.FC<LoginProps> = ({ onLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onLogin(email, password);
+  };
+
+  return (
+    <>
+        <h2>Connexion</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+      <div className="field">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div className="field">
+        <label htmlFor="password">Mot de passe</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </div>
+      <button className="login-form__button" type="submit">
+        Login
+      </button>
+
+     {/*  <Link to="/register">Pas encore inscrit ?</Link> */}
+    </form>
+
+    </>
+   
+  );
+};
+
+export default Admin;
